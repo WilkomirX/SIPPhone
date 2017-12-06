39b82f991fba18dffdaeb8d89e0dac071015e667
David <dawkuczer@gmail.com>
2017-12-06 18:49:01 +0100

    Removing envelope button [DK]

--- app/src/main/java/com/example/shade/sipphone/MainActivity.java
+++ app/src/main/java/com/example/shade/sipphone/MainActivity.java
@@ -19,15 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Hej ho, test jedn dwa trzy   aaa", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
